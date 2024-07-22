9daa328b5dcc3fcc1e2f48c5df661cc097bcccdb
lkm6222 <mombulsala@gmail.com>
2024-07-22 12:32:59 +0900

    <2024.07.22>join예제2 update

--- Join예제2.sql
+++ Join예제2.sql
@@ -34,3 +34,33 @@ ON student.school_id = school.school_id
 GROUP BY student.student_id
 ORDER BY sum(score.score_point) desc
 LIMIT 5;
+
+
+/* 2024년 졸업생과 재학생의 수능 성적 평균을 출력하시오. */
+SELECT  IF(T.student_graduate_yn = 'Y', '졸업생', '재학생') AS '학생구분',
+		AVG(T.sumPoint) AS '평균수능성적'
+FROM 
+		(SELECT  student.student_id,
+				 student.student_name,
+				 student.student_graduate_yn,
+				 SUM(score.score_point) AS sumPoint
+		FROM tb_student_info student
+		INNER JOIN tb_score score
+		ON student.student_id = score.student_id
+		AND score.score_season = '2024'
+		GROUP BY student.student_id) T
+GROUP BY T.student_graduate_yn;
+
+
+SELECT  IF(student.student_graduate_yn = 'Y', '졸업생', '재학생') AS '학생구분',
+		AVG(score.score_point) AS '평균수능성적'
+FROM tb_student_info student
+INNER JOIN (SELECT  student_id, 
+					score_season, 
+                    sum(score_point) AS score_point
+			FROM tb_score
+			WHERE score_season = '2024'
+            GROUP BY student_id) score
+ON student.student_id = score.student_id
+GROUP BY student.student_graduate_yn;
+
